aa9b8af740f0025a3901bc24dc6bd71c7be06288
Kaytlin Borel <kaytlinrb@gmail.com>
2017-03-24 10:56:44 -0700

    add additional branching elements and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 $(function() {
   $("#track-form").submit(function(event) {
     event.preventDefault();
-    var question1= $("#development").val()
-    var question2= $("#produce").val()
-    var question3= $("#workplace").val()
-    var question4= $("#object-orientation").val()
-    var question5= $("#product").val()
+    var question1= $("#development").val();
+    var question2= $("#produce").val();
+    var question3= $("#workplace").val();
+    var question4= $("#object-orientation").val();
+    var question5= $("#product").val();
+
+    if ((question1 === "frontend") && (question5 === "design")) {
+      $("#css").show();
+    } else if ((question1 === "backend") && (question3 === "startup")) {
+      $("#ruby").show();
+    } else if ((question3 === "large-business") && (question2 === "soft")) {
+      $("#csharp").show();
+    } else {
+      $("#any").show();
+    }
 
 
-  })
+  });
 });
