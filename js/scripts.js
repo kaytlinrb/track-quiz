3bc0a2d4896476cc5ccf7eaef77246bb0ae74cd8
Kaytlin Borel <kaytlinrb@gmail.com>
2017-03-24 09:10:53 -0700

    add a little css styling and store variables for dropdown input

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+  $("#track-form").submit(function(event) {
+    event.preventDefault();
+    var question1= $("#development").val()
+    var question2= $("#produce").val()
+    var question3= $("#workplace").val()
+    var question4= $("#object-orientation").val()
+    var question5= $("#product").val()
+
+
+  })
+});
